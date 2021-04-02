db4df6029ddc481381d4d172a01a2ac2647b400e
Mateusz Praski <mateusz.praski@gmail.com>
2021-04-02 15:34:21 +0200

    Added screen buffer

--- main.cpp
+++ main.cpp
@@ -4,21 +4,38 @@
 #define WIDTH 1280
 #define HEIGHT 720
 
+using pixel = unsigned char;
+
 int main() {
     glfwInit();
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "RayMarching", NULL, NULL);
-    if (!window) {
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "RayMarching", nullptr, nullptr);
+    if (window == nullptr) {
         std::cout << "Could not open window";
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
+    auto* buffer = new pixel[WIDTH * HEIGHT * 3];
+
+    for (size_t x = 0; x < WIDTH; ++x) {
+        for (size_t y = 0; y < HEIGHT; ++y) {
+            for (size_t c = 0; c < 3; ++c) {
+                buffer[3*(y * WIDTH + x) + c] = (x+y)*c % 256;
+            }
+        }
+    }
+
     glfwMakeContextCurrent(window);
+    glViewport(0, 0, WIDTH, HEIGHT);
 
     while (!glfwWindowShouldClose(window)) {
+        glDrawPixels(WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, buffer);
 
+        glfwPollEvents();
+        glfwSwapBuffers(window);
     }
 
     glfwDestroyWindow(window);
     glfwTerminate();
+    delete[] buffer;
     exit(EXIT_SUCCESS);
 }
